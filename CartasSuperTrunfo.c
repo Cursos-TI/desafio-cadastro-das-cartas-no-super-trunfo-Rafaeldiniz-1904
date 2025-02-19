3ee9f823b9b2194374fb6c3bbb45add6544e869f
Rafaeldiniz-1904 <202502006403@alunos.estacio.br>
2025-02-19 13:53:33 +0000

    termino do printf e scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
-     // Cadastro das variáveis
+     // Cadastro das variáveis.
 
 int main() {
   char estado1 = 'A', estado2 = 'B';
-  char codigodacarta1 [5] = "01", codigodacarta2[5] = "02";
+  char codigodacarta1 [4] = "01", codigodacarta2[4] = "02";
   char nomedacidade1 [20] = "São Paulo", nomedacidade2 [20] = "Rio de Janeiro";
   int populacao1 = 20, populacao2 = 15;
   float area1 = 32.22, area2 = 25.2;
   float pib1 = 22.22, pib2 = 11.11;
   int pontoturistico1 = 10, pontoturistico2 = 20;
 
-     // Cadastro do printf e scanf das cartas
-
-    scanf("Entre com o Código da carta: \n", &codigodacarta1);
-
-    printf("Estado: %c\nCódigo da carta: %c%s\nNome da Cidade: %s\nPopulação: %d Bilhões de pessoas\nArea: %.2f Km²\nPIB: %.2f Bilhões de reais\nPontos Turísticos: %d\n", estado1, estado1, codigodacarta1, nomedacidade1, populacao1, area1, pib1, pontoturistico1);
+     // Cadastro do printf e scanf das cartas.
+     // Entrar com os códigos A01 enter e depois B02 enter.
+    printf("Entre com o Código da carta 1: \n");
+    scanf("%s", &codigodacarta1);
+    printf("Estado: %c\nCódigo da carta: %s\nNome da Cidade: %s\nPopulação: %d Bilhões de pessoas\nArea: %.2f Km²\nPIB: %.2f Bilhões de reais\nPontos Turísticos: %d\n", estado1, codigodacarta1, nomedacidade1, populacao1, area1, pib1, pontoturistico1);
     
-    printf("Estado: %c\nCódigo da carta: %c%s\nNome da Cidade: %s\nPopulação: %d Bilhões de pessoas\nArea: %.2f Km²\nPIB: %.2f Bilhões de reais\nPontos Turísticos: %d\n", estado2, estado2, codigodacarta2, nomedacidade2, populacao2, area2, pib2, pontoturistico2);
+    printf("Entre com o Código da carta 2: \n");
+    scanf("%s", &codigodacarta2);
+    printf("Estado: %c\nCódigo da carta: %s\nNome da Cidade: %s\nPopulação: %d Bilhões de pessoas\nArea: %.2f Km²\nPIB: %.2f Bilhões de reais\nPontos Turísticos: %d\n", estado2, codigodacarta2, nomedacidade2, populacao2, area2, pib2, pontoturistico2);
 
      
 
