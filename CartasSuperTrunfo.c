6cefd8c89dd58ad2cf94f71c63a2649b298addaf
Rafaeldiniz-1904 <202502006403@alunos.estacio.br>
2025-02-23 19:12:13 +0000

    criação das escolhas de atributos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,8 +5,10 @@
 
 int main() {
 
+
   // Cadastro das variáveis.
   
+
   int opcao;
   char estado1 = 'A', estado2 = 'B';
   char codigodacarta1 [4] = "01", codigodacarta2[4] = "02";
@@ -16,16 +18,22 @@ int main() {
   float pib1 = 11111.22, pib2 = 33333.11;
   int pontosturisticos1 = 20, pontosturisticos2 = 15;
   
+
   //Cadastro da Variavel Dencidade Populacional e PIB per capita
+
 
   float dencidadepopulacional1 = populacao1 / area1, pibpercapita1 = pib1 / populacao1;  
   float dencidadepopulacional2 = populacao2 / area2, pibpercapita2 = pib2 / populacao2;
 
+
   // Criando variavel do jogador e computador
+
 
   int jogador, computador;
   
+
   // Criando o menu de inicialização do jogo
+
 
   printf("*** JOGO SUPER TRUNFO PAÍSES *** \n");
   printf("1.Iniciar Jogo\n");
@@ -35,15 +43,19 @@ int main() {
 
   printf("\n");
 
-  printf("Escolha uma Carta:\n"):
-  printf("1.Carta 1\n", );
+  printf("Escolha uma Carta:\n");
+  printf("1.Carta 1\n");
   printf("2.Carta 2\n");
   scanf("%d", &opcao);
 
   printf("\n");
 
+
   // Criando printf da carta 01
-  
+
+
+  switch (opcao){
+  case 1:
   printf("Carta 01:\n");
   printf("Estado: %c\n", estado1);
   printf("Código da Carta: %c%s\n", estado1, codigodacarta1);
@@ -53,15 +65,28 @@ int main() {
   printf("PIB: %f\n", pib1);
   printf("Números de Pontos Turisticos: %d\n", pontosturisticos1);
 
-  //Criação do printf Dencidade Populacional e PIB per capita carta 01
+//Criação do printf Dencidade Populacional e PIB per capita carta 01
 
   printf("Dencidade Populacional: %f\n", dencidadepopulacional1);
   printf("PIB Per capita: %f\n", pibpercapita1);
-
   printf("\n");
+
+  //Criação da opções de atributos Carta 01
+
+  printf("Escolha um opção:\n");
+  printf("1.População: %d\n", populacao1);
+  printf("2.Área: %f\n", area1);
+  printf("3.PIB: %f\n", pib1);
+  printf("4.Números de Pontos Turisticos: %d\n", pontosturisticos1);
+  printf("5.Dencidade Populacional: %f\n", dencidadepopulacional1);
+  printf("6.PIB Per capita: %f\n", pibpercapita1);
+  scanf("%d", &opcao);
+  
+break;
 
   //Criando printf da carta 02
 
+  case 2:
   printf("Carta 02:\n");
   printf("Estado: %c\n", estado2);
   printf("Código da Carta: %c%s\n", estado2, codigodacarta2);
@@ -75,7 +100,21 @@ int main() {
 
   printf("Dencidade Populacional: %f\n", dencidadepopulacional2);
   printf("PIB Per capita: %f\n", pibpercapita2);
- 
+  printf("\n");
+
+   //Criação da opções de atributos Carta 01
+
+  printf("Escolha um opção:\n");
+  printf("1.População: %d\n", populacao2);
+  printf("2.Área: %f\n", area2);
+  printf("3.PIB: %f\n", pib2);
+  printf("4.Números de Pontos Turisticos: %d\n", pontosturisticos2);
+  printf("5.Dencidade Populacional: %f\n", dencidadepopulacional2);
+  printf("6.PIB Per capita: %f\n", pibpercapita2);
+  scanf("%d", &opcao);
+break;
+  } 
+
   printf("\n");
 
   //Comparação das cartas
